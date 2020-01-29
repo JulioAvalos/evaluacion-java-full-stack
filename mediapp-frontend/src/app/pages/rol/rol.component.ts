d1a9c1b35be0366e1a6c06e2ed80793c992c1ad3
JulioAvalos <julioavalos2@msn.com>
2020-01-28 20:51:09 -0600

    Agregando cambios

--- mediapp-frontend/src/app/pages/rol/rol.component.ts
+++ mediapp-frontend/src/app/pages/rol/rol.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatSnackBar, MatPaginator } from '@angular/material';
+import { Rol } from 'src/app/_model/rol';
+import { RolService } from 'src/app/_service/rol.service';
 
 @Component({
   selector: 'app-rol',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RolComponent implements OnInit {
 
-  constructor() { }
+  cantidad: number = 0;
+  dataSource: MatTableDataSource<Rol>;
+  displayedColumns = ['idRol', 'nombre', 'descripcion', 'acciones'];
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  // @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private rolService: RolService, private snack: MatSnackBar) { }
 
   ngOnInit() {
+
+    this.rolService.rolCambio.subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+      // this.dataSource.paginator = this.paginator;
+    });
+
+    this.rolService.mensajeCambio.subscribe(data => {
+      this.snack.open(data, 'AVISO', {
+        duration: 2000
+      });
+    });
+
+    this.rolService.listarPageable(0, 10).subscribe(data => {
+      this.cantidad = data.totalElements;
+      this.dataSource = new MatTableDataSource(data.content);
+      this.dataSource.sort = this.sort;
+    });
+
+  }
+
+  filtrar(valor: string) {
+    this.dataSource.filter = valor.trim().toLowerCase();
+  }
+
+  mostrarMas(e: any){
+    this.rolService.listarPageable(e.pageIndex, e.pageSize).subscribe(data => {
+      this.cantidad = data.totalElements;
+      this.dataSource = new MatTableDataSource(data.content);
+      this.dataSource.sort = this.sort;
+    });
   }
 
 }
